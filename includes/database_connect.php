8e9e657694f00a163bb9db015d9ea101ecba1125
Pallavi <pallavibhanumurthy123@gmail>
2026-05-14 17:49:05 +0530

    fixed railway database connection

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -1,5 +1,18 @@
 <?php
-$conn = mysqli_connect("127.0.0.1", "root", "", "pglife");
+$conn = mysqli_connect(
+    getenv("MYSQLHOST"),
+    getenv("MYSQLUSER"),
+    getenv("MYSQLPASSWORD"),
+    getenv("MYSQLDATABASE"),
+    getenv("MYSQLPORT")
+);
+
+if (!$conn) {
+    die(json_encode([
+        "success" => false,
+        "message" => mysqli_connect_error()
+    ]));
+}
 
 if (mysqli_connect_errno()) {
     // Throw error message based on ajax or not
